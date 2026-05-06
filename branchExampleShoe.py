c9801fb70d85b35f3b352c11283ba53c6d1c92ba
Shoshana Howard <smhoward1028@gmail.com>
2026-05-06 01:55:47 -0400

    FAHFAHFAH
    attempt number 67 of will this stupid program work

---
+++ branchExampleShoe.py
@@ -0,0 +1,12 @@
+"""
+Name: branchExampleShoe.py
+Coded by: Shoshana Howard
+Purpose: get this stupid program to work
+Date: 5/6/2026
+"""
+
+counter = 0
+
+while (counter<10):
+    print("Time: " + str(counter))
+    counter+=1
